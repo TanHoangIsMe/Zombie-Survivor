5bddec73ccddac36ff7c27b14370ecb9325b4bcb
TanHoang01 <tanhm2k1@gmail.com>
2024-07-14 17:38:38 +0700

    add enemy AI to chase player

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+
+    NavMeshAgent agent;
+    float distanceToTarget = Mathf.Infinity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(transform.position, target.position);
+        if (distanceToTarget <= chaseRange)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Display the explosion radius when selected
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+    }
+}
